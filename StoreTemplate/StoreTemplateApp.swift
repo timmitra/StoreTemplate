d896dbd5cd1beec7ed32eaee87c3f7c605adce5d
Tim Mitra <tim@it-guy.com>
2024-01-16 14:56:22 -0500

    Initial Commit

---
+++ StoreTemplate/StoreTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreTemplateApp.swift
+//  StoreTemplate
+//
+//  Created by Tim Mitra on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct StoreTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
